7040c287837e6108611e17576eed5f19c97b441f
Chris Canterbury <studio@threecrow.co>
2024-09-26 23:10:17 -0700

    init

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,18 @@
 import { defineConfig } from 'astro/config';
 
+import partytown from '@astrojs/partytown';
+import tailwind from '@astrojs/tailwind';
+import netlify from '@astrojs/netlify';
+
+import pocketbase from 'astro-pocketbase';
+
+import sitemap from '@astrojs/sitemap';
+
+import icon from 'astro-icon';
+
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  integrations: [partytown(), tailwind(), pocketbase(), sitemap(), icon()],
+  output: 'server',
+  adapter: netlify()
+});
